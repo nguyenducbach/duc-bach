a4b50bfd54c8fcc4fecdddbd0905409af06c7f5e
VanLeDinh96 <dinhvanle.it@gmail.com>
2024-07-31 20:36:23 +0700

    first commit

---
+++ api/server.js
@@ -0,0 +1,31 @@
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+
+var corsOptions = {
+  origin: process.env.CLIENT_ORIGIN || "http://localhost:8081"
+};
+
+app.use(cors(corsOptions));
+
+app.use(express.json());
+
+app.use(express.urlencoded({ extended: true }));
+
+const db = require("./app/models");
+
+db.sequelize.sync();
+
+app.get("/", (req, res) => {
+  res.json({ message: "Welcome to my application." });
+});
+
+require("./app/routes/turorial.routes")(app);
+
+// set port, listen for requests
+const PORT = process.env.NODE_DOCKER_PORT || 8080;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}.`);
+});
